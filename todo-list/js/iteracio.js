2ccd8bf3f35148a32d72d89a14d487f7cbfdaa57
fesolo2122daw2 <fesolo2122daw2@protonmail.com>
2022-05-06 15:58:27 +0200

    Mostrar tasques al clickar

--- todo-list/js/iteracio.js
+++ todo-list/js/iteracio.js
@@ -6,6 +6,13 @@ window.onload = function () {
     start.style.color = '#f6e9dc';
     start.classList.add('active');
 
+
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    var modalActiu = true;
+
+
     function dotheneedful(sibling) {
         if (sibling != null) {
             start.focus();
@@ -22,38 +29,66 @@ window.onload = function () {
 
     document.onkeydown = checkKey;
 
+    /**
+     * If the user presses the up arrow, the function will check if the cell above the active cell
+     * exists, and if it does, it will make that cell active. 
+     * 
+     * If the user presses the down arrow, the function will check if the cell below the active cell
+     * exists, and if it does, it will make that cell active. 
+     * 
+     * If the user presses the left arrow, the function will check if the cell to the left of the
+     * active cell exists, and if it does, it will make that cell active. 
+     * 
+     * If the user presses the right arrow, the function will check if the cell to the right of the
+     * active cell exists, and if it does, it will make that cell active. 
+     * 
+     * If the user presses the enter key, the function will log the id of the active cell to the
+     * console.
+     * @param e - the event object
+     */
+
     function checkKey(e) {
         e = e || window.event;
-        if (e.keyCode == '38') {
-            // up arrow
-            var idx = start.cellIndex;
-            var nextrow = start.parentElement.previousElementSibling;
-            if (nextrow != null) {
-                var sibling = nextrow.cells[idx];
+        if (modalActiu) {
+            if (e.keyCode == '38') {
+                // up arrow
+                var idx = start.cellIndex;
+                var nextrow = start.parentElement.previousElementSibling;
+                if (nextrow != null) {
+                    var sibling = nextrow.cells[idx];
+                    dotheneedful(sibling);
+                }
+            } else if (e.keyCode == '40') {
+                // down arrow
+                var idx = start.cellIndex;
+                var nextrow = start.parentElement.nextElementSibling;
+                if (nextrow != null) {
+                    var sibling = nextrow.cells[idx];
+                    dotheneedful(sibling);
+                }
+            } else if (e.keyCode == '37') {
+                // left arrow
+                var sibling = start.previousElementSibling;
                 dotheneedful(sibling);
-            }
-        } else if (e.keyCode == '40') {
-            // down arrow
-            var idx = start.cellIndex;
-            var nextrow = start.parentElement.nextElementSibling;
-            if (nextrow != null) {
-                var sibling = nextrow.cells[idx];
+            } else if (e.keyCode == '39') {
+                // right arrow
+                var sibling = start.nextElementSibling;
                 dotheneedful(sibling);
-            }
-        } else if (e.keyCode == '37') {
-            // left arrow
-            var sibling = start.previousElementSibling;
-            dotheneedful(sibling);
-        } else if (e.keyCode == '39') {
-            // right arrow
-            var sibling = start.nextElementSibling;
-            dotheneedful(sibling);
-        } else if (e.keyCode == '13') {
-            var table = document.getElementById("calendar");
-            for (var i = 0, row; row = table.rows[i]; i++) {
-                for (var j = 0, col; col = row.cells[j]; j++) {
-                    if (col.classList.contains('active')) {
-                        console.log(col.id);
+            } else if (e.keyCode == '13') {
+                var table = document.getElementById("calendar");
+                for (var i = 0, row; row = table.rows[i]; i++) {
+                    for (var j = 0, col; col = row.cells[j]; j++) {
+                        if (col.classList.contains('active')) {
+                            let dataABuscar = col.id;
+                            console.log(dataABuscar);
+                            let tasques = JSON.parse(localStorage.getItem(dataABuscar));
+                            console.log(tasques);
+                            if (dataABuscar != null) {
+                                modal.style.display = "block";
+                                modalActiu = !modalActiu;
+                                carregarModal(dataABuscar);
+                            }
+                        }
                     }
                 }
             }
@@ -61,10 +96,42 @@ window.onload = function () {
     }
 
     document.getElementById('calendar').addEventListener('click', (ev) => {
-        console.log(ev.target.id);
+        let dataABuscar = ev.target.id;
+        console.log(dataABuscar);
+        let tasques = JSON.parse(localStorage.getItem(dataABuscar));
+        console.log(tasques);
+        if (dataABuscar != null) {
+            modal.style.display = "block";
+            modalActiu = !modalActiu;
+            carregarModal(dataABuscar);
+        }
+
     })
 
-    document.getElementById('calendar').addEventListener('keydown', (ev) => {
-        console.log(ev.target.id);
-    })
+    span.onclick = function () {
+        modal.style.display = "none";
+        modalActiu = !modalActiu;
+        document.getElementById("tasquesModal").innerHTML = '';
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            modalActiu = !modalActiu;
+            document.getElementById("tasquesModal").innerHTML = '';
+        }
+    }
+
+    function carregarModal(data) {
+        document.getElementById('dataModal').innerText = data;
+        let tasques = JSON.parse(localStorage.getItem(data));
+        if (tasques != null) {
+            let html = "";
+            for (let i = 0; i < tasques.length; i++) {
+                html += "<li>" + tasques[i].titol + " / " + tasques[i].hora + " / " + tasques[i].descripcio + "</li>";
+            }
+            document.getElementById("tasquesModal").innerHTML = html;
+        }
+    }
+
 }
