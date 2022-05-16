e4e358112bf59ed79ab7c0da370adadb14379e3b
fesolo2122daw2 <fesolo2122daw2@protonmail.com>
2022-05-16 17:05:41 +0200

    Esborrar / modificar / editar mitjançant butons

--- todo-list/js/afegir.js
+++ todo-list/js/afegir.js
@@ -1,22 +1,49 @@
 window.onload = function () {
 
-    document.getElementById("afegirTasca").onclick = function () {
-        let data = ""+document.getElementById("dataDia").value+"/"+document.getElementById("dataMes").value+"/"+document.getElementById("dataAny").value;
-        let titol = document.getElementById("titol").value;
-        let hora = ""+document.getElementById("hora").value+":"+document.getElementById("min").value+"";
-        let descripcio = document.getElementById("descripcio").value;
-        let estatTasca = document.getElementById('estatTasca').value;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
 
-        if(localStorage.getItem(data)==null){
-            let tasques = JSON.stringify([{titol: titol, hora: hora, descripcio: descripcio, estatTasca: estatTasca, correuHora: 'no', correuMitja: 'no'}]);
-            console.log(tasques);
-            localStorage.setItem(data, tasques);
-        }else{
-            let tasques = JSON.parse(localStorage.getItem(data));
-            console.log(tasques);
-            tasques.push({titol: titol, hora:hora, descripcio: descripcio, estatTasca: estatTasca, correuHora: 'no', correuMitja: 'no'});
-            localStorage.setItem(data, JSON.stringify(tasques));
+    const data = urlParams.get('data');
+
+    document.getElementById('dataDia').value = data.substring(0, data.indexOf('/'));
+
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++)
+        inputs[i].addEventListener("keydown", function (event) {
+            if (event.keyCode == 227) {
+                if (this.previousElementSibling) {
+                    this.previousElementSibling.focus();
+                }
+            }
+            else if (event.keyCode == 228) {
+                if (this.nextElementSibling) {
+                    this.nextElementSibling.focus();
+                }
+            }
+        }, false);
+
+    window.addEventListener("keydown", function (inEvent) {
+        if (window.event) {
+            keycode = inEvent.keyCode;
         }
-        location.replace('index.html');
-    }
+        if (keycode == 13 || keycode == 406) {
+            let data = "" + document.getElementById("dataDia").value + "/" + document.getElementById("dataMes").value + "/" + document.getElementById("dataAny").value;
+            let titol = document.getElementById("titol").value;
+            let hora = "" + document.getElementById("hora").value + ":" + document.getElementById("min").value + "";
+            let descripcio = document.getElementById("descripcio").value;
+            let estatTasca = document.querySelector('input[name="estatTasca"]:checked').value;
+
+            if (localStorage.getItem(data) == null) {
+                let tasques = JSON.stringify([{ titol: titol, hora: hora, descripcio: descripcio, estatTasca: estatTasca, correuHora: 'no', correuMitja: 'no' }]);
+                console.log(tasques);
+                localStorage.setItem(data, tasques);
+            } else {
+                let tasques = JSON.parse(localStorage.getItem(data));
+                console.log(tasques);
+                tasques.push({ titol: titol, hora: hora, descripcio: descripcio, estatTasca: estatTasca, correuHora: 'no', correuMitja: 'no' });
+                localStorage.setItem(data, JSON.stringify(tasques));
+            }
+            location.replace('index.html');
+        }
+    });
 }
--- todo-list/js/editar.js
+++ todo-list/js/editar.js
@@ -7,7 +7,6 @@ window.onload = function () {
 
     var tasques = JSON.parse(localStorage.getItem(data));
 
-    var estatTascaSelect = document.getElementById('estatTascaModificar');
     var correuHora = tasques[posicio].correuHora;
     var correuMitja = tasques[posicio].correuMitja;
 
@@ -18,30 +17,55 @@ window.onload = function () {
     document.getElementById('descripcioModificar').value = tasques[posicio].descripcio;
 
     if (tasques[posicio].estatTasca == 'acabada') {
-        estatTascaSelect.selectedIndex = "1";
+        document.getElementById('tascaAcabada').checked = true;
     } else {
-        estatTascaSelect.selectedIndex = "0";
+        document.getElementById('tascaNoAcabada').checked = true;
     }
 
-    document.getElementById("modificarTasca").onclick = function () {
-        let data2 = "" + document.getElementById("dataDiaModificar").value + "/" + document.getElementById("dataMesModificar").value + "/" + document.getElementById("dataAnyModificar").value;
-        let titol = document.getElementById("titolModificar").value;
-        let hora = "" + document.getElementById("horaModificar").value + ":" + document.getElementById("minModificar").value + "";
-        let descripcio = document.getElementById("descripcioModificar").value;
-        let estatTasca = document.getElementById('estatTascaModificar').value;
-        tasques.splice(posicio, 1);
-        localStorage.removeItem(data);
-        tasques.push({ titol: titol, hora: hora, descripcio: descripcio, estatTasca: estatTasca, correuHora: correuHora, correuMitja: correuMitja });
-        console.log(tasques);
-        localStorage.setItem(data2, JSON.stringify(tasques));
-        location.replace('index.html');
-    }
+    // document.getElementById("modificarTasca").onclick = function () {
+    //     let data2 = "" + document.getElementById("dataDiaModificar").value + "/" + document.getElementById("dataMesModificar").value + "/" + document.getElementById("dataAnyModificar").value;
+    //     let titol = document.getElementById("titolModificar").value;
+    //     let hora = "" + document.getElementById("horaModificar").value + ":" + document.getElementById("minModificar").value + "";
+    //     let descripcio = document.getElementById("descripcioModificar").value;
+    //     let estatTasca = document.getElementById('estatTascaModificar').value;
+    //     tasques.splice(posicio, 1);
+    //     localStorage.removeItem(data);
+    //     tasques.push({ titol: titol, hora: hora, descripcio: descripcio, estatTasca: estatTasca, correuHora: correuHora, correuMitja: correuMitja });
+    //     console.log(tasques);
+    //     localStorage.setItem(data2, JSON.stringify(tasques));
+    //     location.replace('index.html');
+    // }
 
-    document.getElementById("esborrarTasca").onclick = function () {
-        tasques.splice(posicio, 1);
-        localStorage.removeItem(data);
-        localStorage.setItem(data, JSON.stringify(tasques));
-        location.replace('index.html');
-    }
+    // document.getElementById("esborrarTasca").onclick = function () {
+    //     tasques.splice(posicio, 1);
+    //     localStorage.removeItem(data);
+    //     localStorage.setItem(data, JSON.stringify(tasques));
+    //     location.replace('index.html');
+    // }
+
+    window.addEventListener("keydown", function (inEvent) {
+        if (window.event) {
+            keycode = inEvent.keyCode;
+        }
+        if (keycode == 27 || keycode == 403) {
+            tasques.splice(posicio, 1);
+            localStorage.removeItem(data);
+            localStorage.setItem(data, JSON.stringify(tasques));
+            location.replace('index.html');
+        }
+        else if (keycode == 13 || keycode == 404) {
+            let data2 = "" + document.getElementById("dataDiaModificar").value + "/" + document.getElementById("dataMesModificar").value + "/" + document.getElementById("dataAnyModificar").value;
+            let titol = document.getElementById("titolModificar").value;
+            let hora = "" + document.getElementById("horaModificar").value + ":" + document.getElementById("minModificar").value + "";
+            let descripcio = document.getElementById("descripcioModificar").value;
+            let estatTasca = document.querySelector('input[name="estatTascaModificar"]:checked').value;
+            tasques.splice(posicio, 1);
+            localStorage.removeItem(data);
+            tasques.push({ titol: titol, hora: hora, descripcio: descripcio, estatTasca: estatTasca, correuHora: correuHora, correuMitja: correuMitja });
+            console.log(tasques);
+            localStorage.setItem(data2, JSON.stringify(tasques));
+            location.replace('index.html');
+        }
+    });
 
 }
